faf175846bc59d3ed9a4267d92a5f1732f9081a8
Arthur <avprocopio@gmail.com>
2022-02-04 18:22:50 -0300

    editando app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Meu aplicativo
+          Meu aplicativo editado
         </p>
         <a
           className="App-link"
